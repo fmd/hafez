80b813a13534f51ca295698be31728600c9f7d36
Fareed Dudhia <fareeddudhia@googlemail.com>
2014-07-11 20:47:05 +0100

    rttr

--- main.go
+++ main.go
@@ -3,14 +3,15 @@ package main
 import (
 	"github.com/go-martini/martini"
 	"github.com/martini-contrib/render"
+    "github.com/eknkc/amber"
 )
 
 func main() {
 	m := martini.Classic()
 	m.Use(render.Renderer())
 
-	m.Get("/", func(r render.Render) {
-		r.HTML(200, "home", "")
+	m.Get("/", func() (int, string) {
+		return 200, 
 	})
 
 	m.Run()
