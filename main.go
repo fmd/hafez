cc2e4ed1cd63a6926c1f3a9f685247db988c090d
Fareed Dudhia <fareeddudhia@googlemail.com>
2014-07-11 20:16:22 +0100

    added missing bracket

--- main.go
+++ main.go
@@ -2,12 +2,12 @@ package main
 
 import (
 	"github.com/go-martini/martini"
-    "github.com/martini-contrib/render"
+	"github.com/martini-contrib/render"
 )
 
 func main() {
 	m := martini.Classic()
-    m.Use(render.Renderer()
+	m.Use(render.Renderer())
 
 	m.Get("/", func(r render.Render) string {
 		r.HTML(200, "home")
