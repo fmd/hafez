2c9a4820457b073a62880351a87feab92e6571bd
Fareed Dudhia <fareeddudhia@googlemail.com>
2014-07-14 13:23:41 +0100

    added a page

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bytes"
 	"github.com/eknkc/amber"
 	"github.com/fmd/gin"
 )
@@ -9,20 +8,18 @@ import (
 func main() {
 	r := gin.Default()
 
-	home, err := amber.CompileFile("templates/home.tmpl", amber.Options{})
+	templates, err := amber.CompileDir("templates/", amber.DefaultDirOptions, amber.DefaultOptions)
 	if err != nil {
 		panic(err)
 	}
 
 	r.GET("/", func(c *gin.Context) {
-		var out bytes.Buffer
+		c.ExecHTML(200, templates["home"], nil)
+	})
 
-		err := home.Execute(&out, nil)
-		if err != nil {
-			c.String(500, "Internal Server Error.")
-		}
+	r.GET("/menu", func(c *gin.Context) {
+		c.ExecHTML(200, templates["menu"], nil)
 
-		c.ExecHTML(200, home, nil)
 	})
 
 	r.Run(":5000")
