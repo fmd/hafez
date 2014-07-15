dc0a748cdae88a06e011b348a19429a6e8fb63a5
Fareed Dudhia <fareeddudhia@googlemail.com>
2014-07-15 09:04:16 +0000

    serves static, has middleware

--- main.go
+++ main.go
@@ -3,12 +3,38 @@ package main
 import (
 	"github.com/eknkc/amber"
 	"github.com/fmd/gin"
+	"net/http"
+	"time"
+	"os"
 )
+
+var amberDir string = "templates/"
+var modTime time.Time
+
+func TemplateMod() gin.HandlerFunc {
+	return func (c *gin.Context) {
+		if modTime.IsZero() {
+			modTime = time.Now()
+		}
+
+		info, err := os.Stat(amberDir)
+		if err != nil {
+			modTime = time.Now()
+		}
+
+		if info.ModTime().After(modTime) {
+			modTime = info.ModTime()
+		}
+
+		c.Writer.Header().Set("Last-Modified", modTime.Format(http.TimeFormat))
+	}
+}
 
 func main() {
 	r := gin.Default()
+	r.Use(TemplateMod())
 
-	templates, err := amber.CompileDir("templates/", amber.DefaultDirOptions, amber.DefaultOptions)
+	templates, err := amber.CompileDir(amberDir, amber.DefaultDirOptions, amber.DefaultOptions)
 	if err != nil {
 		panic(err)
 	}
@@ -19,8 +45,9 @@ func main() {
 
 	r.GET("/menu", func(c *gin.Context) {
 		c.ExecHTML(200, templates["menu"], nil)
-
 	})
+
+	r.Static("public/", "./public")
 
 	r.Run(":5000")
 }
