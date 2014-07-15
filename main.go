a2747024e6e0a736d048d6396809a76b32a031e4
Fareed Dudhia <fareeddudhia@googlemail.com>
2014-07-15 18:30:37 +0000

    now has dev middleware

--- main.go
+++ main.go
@@ -3,51 +3,145 @@ package main
 import (
 	"github.com/eknkc/amber"
 	"github.com/fmd/gin"
+	"html/template"
+	"path/filepath"
 	"net/http"
+	"path"
 	"time"
+	"fmt"
 	"os"
 )
 
-var amberDir string = "templates/"
-var modTime time.Time
+type AmberGin struct {
+	TemplateDir string
+	Templates map[string]*template.Template
+	
+	modTime time.Time
+}
 
-func TemplateMod() gin.HandlerFunc {
+func (m *AmberGin) WalkTemplates(path string, info os.FileInfo, err error) error {
+	if info.ModTime().After(m.modTime) {
+		m.modTime = info.ModTime()
+		return nil
+	}
+	return nil
+}
+
+func (m *AmberGin) TemplatesChanged() bool {
+	if m.modTime.IsZero() {
+		m.modTime = time.Now()
+		return true
+	}
+
+	oldMod := m.modTime
+	err := filepath.Walk(m.TemplateDir, m.WalkTemplates)
+
+	if !oldMod.Equal(m.modTime) {
+		return true
+	}
+
+	if err != nil {
+		m.modTime = time.Now()
+		return true
+	}
+
+	return false
+}
+
+func (m *AmberGin) Compile() error {
+	t, err := amber.CompileDir(m.TemplateDir, amber.DefaultDirOptions, amber.DefaultOptions)
+	if err != nil {
+		return err
+	}
+
+	m.Templates = t
+	return nil
+}
+
+func (m *AmberGin) DevMiddleware() gin.HandlerFunc {
 	return func (c *gin.Context) {
-		if modTime.IsZero() {
-			modTime = time.Now()
+		if (m.TemplatesChanged()) {
+			err := m.Compile()	
+			if err != nil {
+				c.String(200, err.Error())
+			}
+
+			c.Writer.Header().Set("Last-Modified", m.modTime.Format(http.TimeFormat))
+		}
+	}
+}
+
+func NewAmberGin(templateDir string) (*AmberGin, error) {
+	m := &AmberGin{}
+	m.TemplateDir = templateDir
+	err := m.Compile()
+	if err != nil {
+		return nil, err
+	}
+
+	return m, nil
+}
+
+func StaticHead(staticUrl string, publicDir string) (string, func(*gin.Context)) {
+	p := path.Join(staticUrl, "/*filepath")
+	return p, func (c *gin.Context) {
+		fp := c.Params.ByName("filepath")
+
+		info, err := os.Stat(path.Join(publicDir, fp))
+		if err != nil || info == nil {
+			c.Abort(404)
+			return
 		}
 
-		info, err := os.Stat(amberDir)
-		if err != nil {
-			modTime = time.Now()
-		}
-
-		if info.ModTime().After(modTime) {
-			modTime = info.ModTime()
-		}
-
-		c.Writer.Header().Set("Last-Modified", modTime.Format(http.TimeFormat))
+		c.Writer.Header().Set("Last-Modified", info.ModTime().Format(http.TimeFormat))
+		c.Abort(200)
+		return
 	}
 }
 
 func main() {
 	r := gin.Default()
-	r.Use(TemplateMod())
 
-	templates, err := amber.CompileDir(amberDir, amber.DefaultDirOptions, amber.DefaultOptions)
+	publicDir := "public"
+	templateDir := "templates"
+	staticUrl := "/assets"
+
+	m, err := NewAmberGin(templateDir)
 	if err != nil {
 		panic(err)
 	}
 
+	r.Use(m.DevMiddleware())
+
 	r.GET("/", func(c *gin.Context) {
-		c.ExecHTML(200, templates["home"], nil)
+
+		data := struct {
+			StaticUrl string
+		} {
+			staticUrl,
+		}
+
+		c.ExecHTML(200, m.Templates["home"], data)
 	})
 
 	r.GET("/menu", func(c *gin.Context) {
-		c.ExecHTML(200, templates["menu"], nil)
+
+		data := struct {
+			StaticUrl string
+		} {
+			staticUrl,
+		}
+
+		c.ExecHTML(200, m.Templates["menu"], data)
 	})
 
-	r.Static("public/", "./public")
+	r.Static(staticUrl, publicDir)
+	r.HEAD(StaticHead(staticUrl, publicDir))
 
-	r.Run(":5000")
+	port := os.Getenv("PORT")
+	if len(port) == 0 {
+		port = "5000"
+	}
+
+	r.Run(fmt.Sprintf(":%s", port))
 }
