6072e10fd56a5cfaa74ea5ec7f8ceeb4002f7749
Fareed Dudhia <fareeddudhia@googlemail.com>
2014-07-16 10:58:30 +0000

    started with gzip assets

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,15 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass')
+var gzip = require('gulp-gzip')
 
 gulp.task('sass', function () {
     gulp.src('./public/scss/*.scss')
         .pipe(sass())
         .pipe(gulp.dest('./public/css'));
+});
+
+gulp.task('compress', function () {
+    gulp.src('./public/css/*.css')
+        .pipe(gzip())
+        .pipe(gulp.dest('./public/zipped/css'));
 });
