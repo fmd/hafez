2a62b5586caf22dd4aaab263c13d136f266f67d7
Fareed Dudhia <fareeddudhia@googlemail.com>
2014-07-18 10:19:46 +0000

    updated main to use the app

--- main.go
+++ main.go
@@ -1,26 +1,5 @@
 package main
 
-import (
-	"os"
-	"fmt"
-	"github.com/codegangsta/negroni"
-	"github.com/julienschmidt/httprouter"
-	"net/http"
-)
-
 func main() {
-	router := httprouter.New()
-	router.GET("/", func(w http.ResponseWriter, req *http.Request, params httprouter.Params) {
-		fmt.Fprintln(w, "Welcome to the homepage using http router")
-	})
-
-	n := negroni.Classic()
-	n.UseHandler(router)
-
-	port := os.Getenv("PORT")
-	if len(port) == 0 {
-		port = "5000"
-	}
-
-	n.Run(":" + port)
+	NewApp(AppOptions{}).Run()
 }
