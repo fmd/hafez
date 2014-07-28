2a5bd67a7b5d1b137959c92a34fa35ab3e6b9be1
Fareed Dudhia <fareeddudhia@googlemail.com>
2014-07-28 16:36:03 +0100

    moved public, fixed up gulpfile;

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,51 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass')
+var less = require('gulp-less')
 var gzip = require('gulp-gzip')
 
+var paths = {
+  scripts: 'static/js/**/*.js',
+  less:    'static/less/**/*.less',
+  sass:    'static/scss/**/*.scss',
+  images:  'static/images/**/*'
+};
+
 gulp.task('sass', function () {
-    gulp.src('./static/scss/*.scss')
+    gulp.src(paths.sass)
         .pipe(sass())
         .pipe(gulp.dest('./public/css'));
 });
 
-gulp.tasj('less', function () {
-    gulp.src('./static/less/*.less')
+gulp.task('less', function () {
+    gulp.src(paths.less)
         .pipe(less())
         .pipe(gulp.dest('./public/css'));
 });
 
+gulp.task('images', function () {
+    gulp.src(paths.images)
+        .pipe(gulp.dest('./public/images'));
+});
+
+gulp.task('scripts', function () {
+    gulp.src(paths.scripts)
+        .pipe(gulp.dest('./public/js'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch(paths.scripts, ['scripts']);
+    gulp.watch(paths.images,  ['images']);
+    gulp.watch(paths.less,    ['less']);
+    gulp.watch(paths.sass,    ['sass']);
+});
+
+gulp.task('default', ['sass', 'less', 'images', 'scripts', 'watch']);
+
+/*
 gulp.task('compress', function () {
     gulp.src('./public/css/*.css')
         .pipe(gzip())
         .pipe(gulp.dest('./public/css'));
 });
+*/
+
